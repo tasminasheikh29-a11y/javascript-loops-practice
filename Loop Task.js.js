c60e7878edc44f64664807e72b1d90085842dbb5
Tasmina <tasminasheikh29@gmail.com>
2025-10-08 18:47:02 +0500

    loop javascript
    This file contains JavaScript code for practicing loops such as for and while.
    It includes examples that show how repetition works in programming.
    The file helps in understanding loop logic and how conditions control the flow of code.
    It is mainly used for testing and displaying loop outputs in the browser.
    This JS file is an important part of learning basic JavaScript concepts.

---
+++ Loop Task.js.js
@@ -0,0 +1,76 @@
+//for loop answer//
+// //Q1//
+// for (let i = 1; i<=20;i+=2){
+//   console.log(i)
+// }
+//Q2//
+// for(let i = 1 ; i<=10 ; i++){
+//     console.log('5 x' + i + '=' + i*5);
+// }
+
+//Q3//
+// for (let i = 10; i >= 1; i--){
+//   console.log(i);
+// }
+
+//Q4//
+// let numbers = [1, 2, 3, 4, 5];
+// for (let num of numbers) {
+//   console.log(`Square of ${num}`);
+// }
+
+//Q6//
+// let name = "Tasmina";
+// for (let i = 0; i < name.length; i++) {
+//   console.log(name[i]);
+// }
+
+//While Loop Answers://
+//Q1//
+// let i = 1;
+// while (i <= 5){
+//   console.log("Hello");
+//     i++;
+// }
+
+//Q2//
+// let number = 1;
+// while (number <=50){
+//   console.log(number);
+//   number++;
+// }
+
+//Q3//
+// let odd = 1;
+// while (odd <= 15){
+//   console.log(odd);
+//   odd += 2;
+// }
+
+//Q4//
+// let i = 1;
+
+// while (i <= 10) {
+//   console.log(`Square of ${i}`);
+//   i++;
+// }
+
+//Q5//
+// let number = parseInt(prompt("Enter a number :"));
+
+// while (number !== 0) {
+//   console.log("You entered:", number);
+//   number = parseInt(prompt("Enter another number (enter 0 to stop):"));
+// }
+
+// console.log("Program ended because you entered 0.");
+
+//Q6//
+// let j = 5;
+
+// while (j >= 1) {
+//   console.log(j);
+//   j--;
+// }
+
+// console.log("Go!");
